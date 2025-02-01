bdcb81c30156ac19db5fdc4915872ad423b6394e
GuidoFC <guidofigueroa96@gmail.com>
2025-02-01 23:08:19 +0100

    añadiendos comentarios al codigo introducido para hacer la autentificacion

---
+++ src/main/java/org/example/apirest/security/SecurityConfig.java
@@ -0,0 +1,53 @@
+package org.example.apirest.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration // Le dice a Spring que esta clase contiene la configuración de seguridad
+@RequiredArgsConstructor // Crea un constructor con todos los campos requeridos, en este caso jwtAuthenticationFilter
+// SecurityConfig Decide qué rutas (URLs) son accesibles sin autenticación
+// Define cómo se valida la identidad de un usuario
+public class SecurityConfig {
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    // Este mét_odo configura cómo manejar las solicitudes entrantes
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/auth/**").permitAll() // Permite acceso sin autenticación a /api/auth
+                        .anyRequest().authenticated() // Cualquier otra solicitud requiere autenticación
+                )
+                .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // JWT no usa sesiones. Con JWT, no se usan sesiones en el servidor; toda la información del usuario está en el token
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class); // El filtro JWT intercepta todas las solicitudes para validar el token antes de permitir el acceso.
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(
+            @Qualifier("userServiceImpl") UserDetailsService userDetailsService) {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(userDetailsService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(provider);
+    }
+}
