589e2bb3d9d1d6a1d758c9599edbfe074e428d7d
GuidoFC <guidofigueroa96@gmail.com>
2025-02-03 08:41:40 +0100

    comentarios para entender mejor esta clase como funciona

--- src/main/java/org/example/apirest/controller/UserController.java
+++ src/main/java/org/example/apirest/controller/UserController.java
@@ -8,8 +8,13 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "*")
+// UserController: Maneja las operaciones CRUD de usuarios (registro, actualización, eliminación, etc.).
+// UserDto: Representa un usuario cuando se devuelve desde la API (por ejemplo, al consultar un usuario).
+// CreateUserDto: Representa los datos necesarios para crear o actualizar un usuario
+// Si necesitas agregar un endpoint específico para usuarios (por ejemplo, buscar usuarios por rol),
+// puedes sobrescribir o extender los métodos de la clase base
 public class UserController extends GeneralizedController<UserDto, CreateUserDto> {
     public UserController(UserServiceImpl service) {
-        super(service);
+        super(service); // Pasa la instancia del servicio a la clase padre (GeneralizedController).
     }
 }
