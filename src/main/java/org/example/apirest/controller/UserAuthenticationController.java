6b31e696133c23825673241cac75f6fd6b6278d5
GuidoFC <guidofigueroa96@gmail.com>
2025-02-03 18:36:13 +0100

    cambio el nombre a uno mas descriptivo

--- src/main/java/org/example/apirest/controller/UserAuthenticationController.java
+++ src/main/java/org/example/apirest/controller/UserAuthenticationController.java
@@ -1,20 +1,14 @@
 package org.example.apirest.controller;
 
 import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import org.example.apirest.dto.role.RoleDto;
 import org.example.apirest.dto.user.CreateUserDto;
 import org.example.apirest.dto.user.UserDto;
-import org.example.apirest.dto.userHasRole.UserHasRoleDto;
 import org.example.apirest.security.JwtKeyProvider;
 import org.example.apirest.service.user.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
-import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 import java.util.List;
@@ -23,14 +17,14 @@ import java.util.Optional;
 @RestController // Todos los métodos devolverán respuestas HTTP. Por defecto, todos los métodos devolverán un estado 200 y en formato JSON.
 @RequestMapping("/api/auth") // Todos los endpoints de este controlador tendrán la ruta /api/auth
 @CrossOrigin(origins = "*") // Permite que los endpoints de este controlador puedan ser accedidos por cualquier dominio
-public class AuthController {
+public class UserAuthenticationController {
 
     private final UserServiceImpl userService; // servicio para buscar y guardar usuarios en la base de datos
     private final PasswordEncoder passwordEncoder; // Clase de Spring Security para encriptar y verificar contraseñas
     private final JwtKeyProvider jwtKeyProvider;
 
 
-    public AuthController(UserServiceImpl userService, PasswordEncoder passwordEncoder, JwtKeyProvider jwtKeyProvider) {
+    public UserAuthenticationController(UserServiceImpl userService, PasswordEncoder passwordEncoder, JwtKeyProvider jwtKeyProvider) {
         this.userService = userService;
         this.passwordEncoder = passwordEncoder;
         this.jwtKeyProvider = jwtKeyProvider;
