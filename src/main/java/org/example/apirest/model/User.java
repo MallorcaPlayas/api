5dcca91e9dcd663ee975ef07489233370e55cad6
GuidoFC <guidofigueroa96@gmail.com>
2025-02-07 09:14:53 +0100

    cambiar el nombre de las columnas

--- src/main/java/org/example/apirest/model/User.java
+++ src/main/java/org/example/apirest/model/User.java
@@ -20,17 +20,41 @@ public class User implements BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
 
+    @Column(name = "user_name") // Ajuste al nombre de la columna en la base de datos
     private String userName;
+
+    @Column(name = "first_surname") // Ajuste al nombre de la columna en la base de datos
     private String firstSurname;
+
+    @Column(name = "second_surname") // Ajuste al nombre de la columna en la base de datos
     private String secondSurname;
+
     private String email;
+
     private Date birthday;
+
     private String password;
-    private String urlPhoto;
+
+    @Column(name = "profile_picture") // Ajuste al nombre de la columna en la base de datos
+    private String profilePicture; // Cambiado de `urlPhoto` a `profilePicture` para coincidir con la base de datos
+
+    @Column(name = "private_privacy") // Ajuste al nombre de la columna en la base de datos
     private boolean privatePrivacy;
+
     private boolean state;
+
+    @ManyToOne
+    @JoinColumn(name = "organization_id", nullable = true)
+    private Organization organization;
+
+    @Column(name = "email_verified_at", nullable = true)
+    private Date emailVerifiedAt;
+
+    @Column(name = "remember_token", nullable = true)
+    private String rememberToken;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL , orphanRemoval = true)
     private List<UserHasRole> roles;
@@ -47,8 +71,6 @@ public class User implements BaseEntity {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL , orphanRemoval = true)
     private List<Excursion> excursions;
 
-    @ManyToOne
-    private Organization organization;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL , orphanRemoval = true)
     private List<Ticket> tickets;
