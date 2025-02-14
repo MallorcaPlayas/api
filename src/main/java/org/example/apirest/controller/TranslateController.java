b8911b97a65706ba6380e10f76ce9029ac180f9f
GuidoFC <guidofigueroa96@gmail.com>
2025-02-14 09:16:00 +0100

    devolver json que tengo en local a quasar

--- src/main/java/org/example/apirest/controller/TranslateController.java
+++ src/main/java/org/example/apirest/controller/TranslateController.java
@@ -1,9 +1,14 @@
 package org.example.apirest.controller;
 
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.example.apirest.service.TranslatorProvider;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.Map;
 
 @RestController
@@ -28,13 +33,47 @@ public class TranslateController {
 
     // http://localhost:8080/translator/translateJsonAsText?origen=es&translated=en
     @PostMapping("/translateJsonAsText")
-    public Map<String, Object> translateJsonAsText(@RequestBody Map<String, Object> json,
-                                      @RequestParam String origen,
-                                      @RequestParam String translated) {
-        // Map<String, Object> sirve para convertir un JSON recibido en una estructura de datos manejable en Java
-        // Aqui el Map<String, Object> json es un JSON que se envia en el cuerpo de la solicitud
+    // ResponseEntity<Map<String, Object>> → Indica que el método devolverá un JSON en la respuesta.
+    // Que es ResponseEntity ? → Es una clase de Spring que permite devolver un status code, headers, and body
+    public ResponseEntity<Map<String, Object>> translateJson(
+            @RequestBody Map<String, Object> json,
+            @RequestParam String origen,
+            @RequestParam String translated) {
+
+        Map<String, Object> translatedJson = translatorProvider.translateJsonAsText(json, origen, translated);
 
 
-        return translatorProvider.translateJsonAsText(json, origen, translated);
+        return ResponseEntity.ok() // ResponseEntity.ok() → Devuelve un HTTP 200 (OK) si to_do salió bien.
+                .contentType(MediaType.APPLICATION_JSON) // contentType(MediaType.APPLICATION_JSON) → Indica que el contenido de la respuesta es un JSON.
+                .body(translatedJson); //.body(translatedJson) → Envía el JSON traducido en la respuesta.
+    }
+
+    @GetMapping("/getTranslatedJson")
+    public ResponseEntity<Map<String, Object>> getTranslatedJson(
+            @RequestParam String origen,
+            @RequestParam String translated) {
+
+        String directoryPath = "src/main/java/org/example/apirest/ABorrar";
+//        String fileName = "translated_" + origen.toUpperCase() + "_to_" + translated.toUpperCase() + ".json";
+        String fileName;
+        if ("es-ES".equalsIgnoreCase(translated)) {
+            fileName = "jsonIndiceEspa.json";
+        } else {
+            fileName = "translated_" + origen.toUpperCase() + "_to_" + translated.toUpperCase() + ".json";
+        }
+        File jsonFile = new File(directoryPath, fileName); // Representa el archivo JSON que queremos leer.
+
+        if (jsonFile.exists()) {
+            try {
+                // ObjectMapper es una libreria que sirve para convertir JSON a Map y viceversa
+                ObjectMapper objectMapper = new ObjectMapper();
+                Map<String, Object> jsonData = objectMapper.readValue(jsonFile, Map.class); // (readValue) Convierte el JSON en un objecto Java
+                return ResponseEntity.ok(jsonData);
+            } catch (IOException e) {
+                return ResponseEntity.internalServerError().build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
