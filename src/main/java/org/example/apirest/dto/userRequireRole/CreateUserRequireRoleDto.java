1ec371a36f0692d0521d329c05a76b3399582067
alexml47 <amoldovan485@alumnes.politecnicllevant.cat>
2025-02-24 14:21:27 +0100

    Fixed User-require-role endpoint

--- src/main/java/org/example/apirest/dto/userRequireRole/CreateUserRequireRoleDto.java
+++ src/main/java/org/example/apirest/dto/userRequireRole/CreateUserRequireRoleDto.java
@@ -3,13 +3,14 @@ package org.example.apirest.dto.userRequireRole;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 
 @Data
 public class CreateUserRequireRoleDto {
     private Long user_id;
     private Long role_id;
-    private String urlPhotoDni;
-    private String urlOfficialDoc;
+    private LocalDateTime created;
+    private LocalDateTime updated;
     private boolean isApproved;
 }
